86f1bd7837a21e2b18ea1944282d4456c9ac074e
DSieghard <86571071+DSieghard@users.noreply.github.com>
2021-12-06 17:01:05 -0300

    TODO: Report Critical Fail on RegisterActivity.kt Added Offer Activity with Spinner and RatingBar, TODO Linkin

--- app/src/main/java/com/sgtst/proyect_cobra/OfferActivity.kt
+++ app/src/main/java/com/sgtst/proyect_cobra/OfferActivity.kt
@@ -1,16 +1,21 @@
 package com.sgtst.proyect_cobra
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class OfferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_offer)
-        val show_context = Toast.makeText(this, "WIP", Toast.LENGTH_SHORT).show()
+
+        val textView2 = findViewById<View>(R.id.textView2) as TextView
+
         val spinner: Spinner = findViewById(R.id.spinner)
         ArrayAdapter.createFromResource(
             this,
@@ -19,6 +24,61 @@ class OfferActivity : AppCompatActivity() {
         ).also { arrayAdapter ->
             arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = arrayAdapter
+        }
+
+
+        val context = this
+        val list = listOf(
+            "Value 01",
+            "Value 02",
+            "Value 03",
+            "Value 04"
+        )
+
+        val adapter:ArrayAdapter<String> = object: ArrayAdapter<String>(
+            context,
+            android.R.layout.simple_spinner_dropdown_item,
+            list
+        ){
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view:TextView = super.getDropDownView(
+                    position,
+                    convertView,
+                    parent
+                ) as TextView
+
+                view.setTypeface(view.typeface, Typeface.BOLD)
+
+                if(position == spinner.selectedItemPosition){
+                    view.background = ColorDrawable(Color.parseColor("#FAEBD7"))
+                    view.setTextColor(Color.parseColor("#008000"))
+                }
+                return view
+            }
+        }
+
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                textView2.text = "Selected: "
+                // get selected item text
+                textView2.append(parent.getItemAtPosition(position).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // another interface callback
+            }
         }
     }
 }
